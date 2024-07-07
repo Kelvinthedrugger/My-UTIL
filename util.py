b43ae8ad535033a40d4a856cfa298bc468db062e
Kelvin Lee <59814445+Kelvinthedrugger@users.noreply.github.com>
2024-07-07 14:14:23 +0800

    Update util.py: add default filename from url in function 'download_file'

--- util.py
+++ util.py
@@ -5,13 +5,15 @@
 # ref: https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
 def download_file(url, fname=""):
     import requests
-    #fname = url.split('/')[-1]
+    if len(fname) = "":
+        # TODO: consider to check for existing files here?
+        fname = url.split('/')[-1]
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(fname, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
+                # If you have chunk encoded response uncomment 'if'
                 # and set chunk_size parameter to None.
                 if chunk:
                     f.write(chunk)
